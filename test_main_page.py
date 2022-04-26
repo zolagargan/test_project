6c148eba58c6afa5d73bafdefaad1672c08c74af
zolagargan <yellowflat@gmail.com>
2022-04-26 14:17:07 +0300

    added login_page edited locators and test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,21 @@
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+#def test_guest_can_go_to_login_page(browser):
+#    link = "http://selenium1py.pythonanywhere.com"
+#    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+#    page.open()                      # открываем страницу
+#    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
+#def test_guest_should_see_login_link(browser):
+#    link = "http://selenium1py.pythonanywhere.com"
+#    page = MainPage(browser, link)
+#    page.open()
+#    page.should_be_login_link()
+
+def test_guest_should_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.should_be_login_page()

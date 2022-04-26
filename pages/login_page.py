3ab63d878dd89a063ec12d3f478c956496c2828e
zolagargan <yellowflat@gmail.com>
2022-04-26 14:33:27 +0300

    в тестовом файле реализован переход с main_page на login_page

--- pages/login_page.py
+++ pages/login_page.py
@@ -7,12 +7,15 @@ class LoginPage(BasePage):
         self.should_be_login_form()
         self.should_be_register_form()
 
+
     def should_be_login_url(self):
         # подстрока "login" есть в текущем url браузера.
         assert "login" in self.browser.current_url, "wrong login_url"
 
+
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
